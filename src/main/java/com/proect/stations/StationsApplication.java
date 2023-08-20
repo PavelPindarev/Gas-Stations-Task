4cb9abd69128ed2aa4f7c27abae805b24e9a1ef0
PavelPindarev <pavel09.1970@abv.bg>
2023-08-20 19:50:07 +0300

    Initial Commit.

---
+++ src/main/java/com/proect/stations/StationsApplication.java
@@ -0,0 +1,13 @@
+package com.proect.stations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationsApplication.class, args);
+	}
+
+}
