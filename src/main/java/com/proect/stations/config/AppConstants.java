9fdf6e2d7cd148941e8f1aafeba1a51ce036698e
PavelPindarev <pavel09.1970@abv.bg>
2023-08-23 14:54:34 +0300

    added swagger

--- src/main/java/com/proect/stations/config/AppConstants.java
+++ src/main/java/com/proect/stations/config/AppConstants.java
@@ -1,8 +1,10 @@
 package com.proect.stations.config;
 
 public final class AppConstants {
+
     private AppConstants() {
     }
+    public static final String PROJECT_NAME = "pro-ect_task";
     public static final String JSON_URL = "https://wejago.de/assets/data/gas-stations-data.json";
     public static final String API_BASE = "/api/v1";
 }
